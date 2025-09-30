4c44ab60bd3c3f19ee977ad7038685f642090fc7
PatrykW7 <patryk90020@o2.pl>
2025-09-30 20:04:25 +0200

    Starting Commit

---
+++ olist_pro/main.py
@@ -0,0 +1,12 @@
+#def main():
+#    print("Hello from olist-pro!")
+
+
+#if __name__ == "__main__":
+#    main()
+import pandas as pd 
+
+data = {"a":[1,2,3],
+        "b":[4,5,6]}
+df = pd.DataFrame(data = data)
+print(df.head())
